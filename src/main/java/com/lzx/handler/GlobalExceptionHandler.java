b63a5723ed99bf33f104112c7a96e3862e5d6451
lizhengxing <1241276517@qq.com>
2025-09-22 16:16:36 +0800

    异常处理

---
+++ src/main/java/com/lzx/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.lzx.handler;
+
+import com.lzx.exception.BaseException;
+import com.lzx.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器，处理项目中抛出的业务异常
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获业务异常
+     * @param ex 业务异常
+     * @return Result 异常信息
+     */
+    @ExceptionHandler(BaseException.class)
+    public Result<String> exceptionHandler(BaseException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.fail(ex.getMessage());
+    }
+
+}
